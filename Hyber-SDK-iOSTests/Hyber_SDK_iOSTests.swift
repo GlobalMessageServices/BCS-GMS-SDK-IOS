43e882d67ec4cf9c14a68da30711da4fa20e0d2f
Dzmitry Buynovskiy <imperituro.ard@gmail.com>
2019-11-24 20:41:55 +0300

    init

---
+++ Hyber-SDK-iOSTests/Hyber_SDK_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hyber_SDK_iOSTests.swift
+//  Hyber-SDK-iOSTests
+//
+//  Created by Дмитрий Буйновский on 24/11/2019.
+//  Copyright © 2019 GMS. All rights reserved.
+//
+
+import XCTest
+@testable import Hyber_SDK_iOS
+
+class Hyber_SDK_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
